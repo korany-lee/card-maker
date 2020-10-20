f7cbdb41f0cc48d5a38b0d6bd4ea7c32c1f8a534
Koeun Lee <korany@kakao.com>
2020-10-20 09:30:35 +0900

    complete login UI

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,8 +1,12 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
+//Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
