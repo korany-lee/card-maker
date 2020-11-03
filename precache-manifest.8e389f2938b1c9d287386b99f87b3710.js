d9d5cf6f4bfb2efbe18c98c8c3a011f463f7e4f1
Koeun Lee <korany@kakao.com>
2020-11-03 11:40:34 +0900

    Updates

---
+++ precache-manifest.8e389f2938b1c9d287386b99f87b3710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990e096c46f6f2b05bea32b54a33b2f5",
+    "url": "/card-maker/index.html"
+  },
+  {
+    "revision": "211f1145dcacde27abdf",
+    "url": "/card-maker/static/css/main.93532b99.chunk.css"
+  },
+  {
+    "revision": "ab769078508d8d8b6b5b",
+    "url": "/card-maker/static/js/2.0ead7888.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/card-maker/static/js/2.0ead7888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211f1145dcacde27abdf",
+    "url": "/card-maker/static/js/main.00c67972.chunk.js"
+  },
+  {
+    "revision": "7100c9d9ce85d7d659c7",
+    "url": "/card-maker/static/js/runtime-main.6126bd76.js"
+  }
+]);
